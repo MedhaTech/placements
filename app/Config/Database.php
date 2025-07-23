b4e06f47ae1d5df491795969ab27145292c3c301
geetha-medha <geethanjali@medhatech.in>
2025-07-23 17:43:48 +0530

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -25,7 +25,7 @@ class Database extends Config
      * @var array<string, mixed>
      */
 
-    public array $default = [
+    /*public array $default = [
 
         'DSN'          => '',
         'hostname'     => 'localhost',
@@ -51,11 +51,11 @@ class Database extends Config
             'datetime' => 'Y-m-d H:i:s',
             'time'     => 'H:i:s',
         ],
-    ];
+    ]; */
 
     #for live server, you can use the following database settings.
     
-    /*public array $default = [
+    public array $default = [
         'DSN'        => '',
         'hostname'   => '192.185.129.71',
         'username'   => 'medha_placements',
@@ -75,7 +75,7 @@ class Database extends Config
         'datetime' => 'Y-m-d H:i:s',
         'time'     => 'H:i:s',
          ],
-    ];*/
+    ];
     //    /**
     //     * Sample database connection for SQLite3.
     //     *
